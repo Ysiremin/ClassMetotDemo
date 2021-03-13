58cd05e2d19ee3f1da8a140c29e50e8965ee515e
Yashiru <yasireminciftci@gmail.com>
2021-03-13 14:39:48 +0300

    .

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,6 +6,7 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
+
             Musteri musteri1 = new Musteri();
             musteri1.Id = "01";
             musteri1.Name = "Yasir Emin";
